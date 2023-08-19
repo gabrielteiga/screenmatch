088d434e9da4a028343cbb066b32b758f560d3e9
Gabriel Teiga <gabrielteiga99@gmail.com>
2023-08-19 18:35:44 -0300

    Feature finalizada, edicao de um filme dentro do banco de dados

--- src/main/java/br/com/gabriel/screenmatch/controller/FilmeController.java
+++ src/main/java/br/com/gabriel/screenmatch/controller/FilmeController.java
@@ -1,15 +1,14 @@
 package br.com.gabriel.screenmatch.controller;
 
+import br.com.gabriel.screenmatch.domain.filme.DadosAlteracaoFilme;
 import br.com.gabriel.screenmatch.domain.filme.DadosCadastroFilme;
 import br.com.gabriel.screenmatch.domain.filme.Filme;
 import br.com.gabriel.screenmatch.domain.filme.FilmeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/filmes")
@@ -34,13 +33,24 @@ public class FilmeController {
     }
 
     @PostMapping
+    @Transactional
     public String cadastraFilme(DadosCadastroFilme dados) {
         var filme = new Filme(dados);
         repository.save(filme);
         return "redirect:/filmes";
     }
 
+    @PutMapping
+    @Transactional
+    public String alteraFilme(DadosAlteracaoFilme dados) {
+        var filme = repository.getReferenceById(dados.id());
+        filme.atualizaDados(dados);
+
+        return "redirect:/filmes";
+    }
+
     @DeleteMapping
+    @Transactional
     public String removeFilme(Long id){
         repository.deleteById(id);
         return "redirect:/filmes";
--- src/main/java/br/com/gabriel/screenmatch/domain/filme/Filme.java
+++ src/main/java/br/com/gabriel/screenmatch/domain/filme/Filme.java
@@ -49,4 +49,11 @@ public class Filme {
                 ", genero='" + genero + '\'' +
                 '}';
     }
+
+    public void atualizaDados(DadosAlteracaoFilme dados) {
+        this.nome = dados.nome();
+        this.duracaoEmMinutos = dados.duracao();
+        this.anoLancamento = dados.ano();
+        this.genero = dados.genero();
+    }
 }
